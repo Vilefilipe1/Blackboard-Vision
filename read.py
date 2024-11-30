33e84f0bfed7446ff5b471140a08dc8a671964d6
Filipe Ciríaco <filipeciriaco0@gmail.com>
2024-11-30 16:44:24 -0300

    Add requirements for pip install and changed small things

--- read.py
+++ read.py
@@ -1,7 +1,9 @@
-import numpy as np
 import os
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+import numpy as np
 import tensorflow as tf
-# from tensorflow import keras
 import glob
 import shutil
 from datetime import datetime
@@ -12,7 +14,7 @@ img_height = 180
 img_width = 180
 class_names = ['etc', 'whiteboard']
 array_files_scanned = []
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 
 
 config = configparser.ConfigParser()
@@ -20,10 +22,10 @@ try:
     os.mkdir("./user-settings")
     print("Not found user settings, creating a new one...")
     os.system('cls')
-    print("Example: D:/Arquivos e Programas/Celular/Gallery/Camera")
+    print("Example: C:/Users/User/Desktop/Camera/gallery")
     directory_user_camera = input("What directory of images to use? (Folder to filter out): ")
     os.system('cls')
-    print("Example: D:/Arquivos e Programas/Celular/Pictures/Gallery/owner/Quadro")
+    print("Example: C:/Users/User/Desktop/Camera/filtered")
     directory_user_whiteboard = input("What directory to place images filtered? (Folder organized): ")
     is_cloud = input("Want cloud backup?? : (y, [N]) ")
     if ((is_cloud == "y") or (is_cloud == "yes") or (is_cloud == "Yes") or (is_cloud == "YES")):
@@ -51,24 +53,25 @@ except FileExistsError:
 
 
 def classSchedule(a):
-    a = a.removeprefix(directory_user_camera + "\\IMG_")
+    a = a.removeprefix(directory_user_camera + "\\IMG")
+    a = a[1:99999]
     horario = a[(a.find("_") + 1):(a.find(".jpg") - 2)]
     a = a.replace(a[a.find("_"):(a.find(".jpg") + 4)], "")
     given_date = datetime(int(a[0:4]), int(a[4:6]), int(a[6:8]))
     day_of_week = given_date.isoweekday() % 7 
-    print(day_of_week)
     hour = int(horario[0:2])
     min = int(horario[2:4])
+    print("day of the week:{}  Time:{}:{}".format(day_of_week,hour, min))
     if (day_of_week == 1) or (day_of_week == 3):
-        # Segunda ou Quarta
+        # Monday ou Wednesday 
         if (hour < 15) or ((hour == 15) and (min <= 10)):
             return "PI"  
-        if (hour <= 17):
+        if (hour < 17) or ((hour <= 17) and (min <= 15)):
             return "LP"           
         else:
             return "C-B"
     if (day_of_week == 2) or (day_of_week == 4):
-        # Terça ou Quinta
+        # Tuesday ou Thursday
         if (hour < 15) or (hour == 15) and (min <= 10):
             return "F-1"            
         else:
@@ -86,7 +89,7 @@ def load_img (img):
     return class_names[np.argmax(score)]
 
 def load_directory ():
-    list = glob.glob(directory_user_camera + "\*.jpg")
+    list = glob.glob(directory_user_camera + "/*.jpg")
     for n in list:
         try:
             if (array_files_scanned.index(n) != 0):
@@ -113,7 +116,7 @@ def load_directory ():
         config.write(configfile)
 
 # Recreate the exact same model, including its weights and the optimizer
-model = tf.keras.models.load_model("./training/my_model_11-47_29-11.keras") 
+model = tf.keras.models.load_model("./training/my_model_15-5_30-11.keras") 
 
 load_directory()
 
